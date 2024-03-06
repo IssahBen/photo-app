f13f212dda2ea014507172c7a3084fba6ae8ee4c
IssahBen <benedictissah@gmail.com>
2024-03-06 05:53:08 -0600

    initial commit .Stipe js not working

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  has_one :payment
+  accepts_nested_attributes_for :payment
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :validatable
+end
